68123e71587e0c21a113b448617f8925142787e8
RabindraD234 <drabindra978@gmail.com>
2025-06-14 02:32:59 +0530

    Initial working version of LimaScope with Docker integration

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"embed"
 	"io/fs"
-
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
-	"path/filepath"
 	"syscall"
 	"time"
 
@@ -27,23 +26,19 @@ import (
 //go:embed all:dist
 var content embed.FS
 
-//go:embed shared_cert.pem shared_key.pem
-var certs embed.FS
-
-//go:generate protoc --go_out=. --go-grpc_out=. --proto_path=./protos ./protos/rpc.proto ./protos/types.proto
 func main() {
 	cli.ValidateEnvVars(cli.Args{}, cli.AgentCmd{})
 	args, subcommand := cli.ParseArgs()
+
 	if subcommand != nil {
 		runnable, ok := subcommand.(cli.Runnable)
 		if !ok {
 			log.Fatal().Msg("Invalid command")
 		}
-		err := runnable.Run(args, certs)
+		err := runnable.Run(args, content)
 		if err != nil {
 			log.Fatal().Err(err).Msg("Failed to run command")
 		}
-
 		os.Exit(0)
 	}
 
@@ -54,36 +49,37 @@ func main() {
 	log.Info().Msgf("Dozzle version %s", args.Version())
 
 	var hostService web.HostService
-	if args.Mode == "server" {
-
-		multiHostService := cli.CreateMultiHostService(certs, args)
+	switch args.Mode {
+	case "server":
+		multiHostService := cli.CreateMultiHostService(content, args)
 		if multiHostService.TotalClients() == 0 {
 			log.Fatal().Msg("Could not connect to any Docker Engine")
-		} else {
-			log.Info().Int("clients", multiHostService.TotalClients()).Msg("Connected to Docker")
 		}
+		log.Info().Int("clients", multiHostService.TotalClients()).Msg("Connected to Docker")
 		hostService = multiHostService
-	} else if args.Mode == "swarm" {
+
+	case "swarm":
 		localClient, err := docker.NewLocalClient("")
 		if err != nil {
 			log.Fatal().Err(err).Msg("Could not create docker client")
 		}
-		certs, err := cli.ReadCertificates(certs)
-		if err != nil {
-			log.Fatal().Err(err).Msg("Could not read certificates")
-		}
-		agentManager := docker_support.NewRetriableClientManager(args.RemoteAgent, args.Timeout, certs)
-		manager := docker_support.NewSwarmClientManager(localClient, certs, args.Timeout, agentManager, args.Filter)
+
+		dummyCert := tls.Certificate{}
+		agentManager := docker_support.NewRetriableClientManager(args.RemoteAgent, args.Timeout, dummyCert)
+		manager := docker_support.NewSwarmClientManager(localClient, dummyCert, args.Timeout, agentManager, args.Filter)
 		hostService = docker_support.NewMultiHostService(manager, args.Timeout)
+
 		log.Info().Msg("Starting in swarm mode")
 		listener, err := net.Listen("tcp", ":7007")
 		if err != nil {
 			log.Fatal().Err(err).Msg("failed to listen")
 		}
-		server, err := agent.NewServer(localClient, certs, args.Version(), args.Filter)
+
+		server, err := agent.NewServer(localClient, dummyCert, args.Version(), args.Filter)
 		if err != nil {
 			log.Fatal().Err(err).Msg("failed to create agent")
 		}
+
 		go cli.StartEvent(args, "swarm", localClient, "")
 		go func() {
 			log.Info().Msgf("Dozzle agent version in swarm mode %s", args.Version())
@@ -91,7 +87,8 @@ func main() {
 				log.Error().Err(err).Msg("failed to serve")
 			}
 		}()
-	} else if args.Mode == "k8s" {
+
+	case "k8s":
 		localClient, err := k8s.NewK8sClient(args.Namespace)
 		if err != nil {
 			log.Fatal().Err(err).Msg("Could not create k8s client")
@@ -104,7 +101,8 @@ func main() {
 
 		go cli.StartEvent(args, "k8s", localClient, "")
 		hostService = clusterService
-	} else {
+
+	default:
 		log.Fatal().Str("mode", args.Mode).Msg("Invalid mode")
 	}
 
@@ -115,12 +113,14 @@ func main() {
 			log.Fatal().Err(err).Msg("failed to listen")
 		}
 	}()
+
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
 	<-ctx.Done()
 	stop()
 	log.Info().Msg("shutting down gracefully, press Ctrl+C again to force")
+
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
@@ -130,11 +130,8 @@ func main() {
 }
 
 func fileExists(filename string) bool {
-    _, err := os.Stat(filename)
-    if os.IsNotExist(err) {
-        return false
-    }
-    return err == nil
+	_, err := os.Stat(filename)
+	return !os.IsNotExist(err)
 }
 
 func createServer(args cli.Args, hostService web.HostService) *http.Server {
@@ -142,30 +139,25 @@ func createServer(args cli.Args, hostService web.HostService) *http.Server {
 
 	var provider web.AuthProvider = web.NONE
 	var authorizer web.Authorizer
+
 	if args.AuthProvider == "forward-proxy" {
-		log.Debug().Msg("Using forward proxy authentication")
 		provider = web.FORWARD_PROXY
 		authorizer = auth.NewForwardProxyAuth(args.AuthHeaderUser, args.AuthHeaderEmail, args.AuthHeaderName, args.AuthHeaderFilter)
 	} else if args.AuthProvider == "simple" {
-		log.Debug().Msg("Using simple authentication")
 		provider = web.SIMPLE
-		
-        userFilePath := "./data/users.yml"
-        if !fileExists(userFilePath) {
-            userFilePath = "./data/users.yaml"
-            if !fileExists(userFilePath) {
-                log.Fatal().Msg("No users.yaml or users.yml file found.")
-            }
-        }
 
-        log.Debug().Msgf("Reading %s file", filepath.Base(userFilePath))
+		userFilePath := "./data/users.yml"
+		if !fileExists(userFilePath) {
+			userFilePath = "./data/users.yaml"
+			if !fileExists(userFilePath) {
+				log.Fatal().Msg("No users.yaml or users.yml file found.")
+			}
+		}
 
-        db, err := auth.ReadUsersFromFile(userFilePath)
-        if err != nil {
-            log.Fatal().Err(err).Msgf("Could not read users file: %s", userFilePath)
-        }
-
-		log.Debug().Int("users", len(db.Users)).Msg("Loaded users")
+		db, err := auth.ReadUsersFromFile(userFilePath)
+		if err != nil {
+			log.Fatal().Err(err).Msgf("Could not read users file: %s", userFilePath)
+		}
 		ttl := time.Duration(0)
 		if args.AuthTTL != "session" {
 			ttl, err = time.ParseDuration(args.AuthTTL)
@@ -177,7 +169,6 @@ func createServer(args cli.Args, hostService web.HostService) *http.Server {
 	}
 
 	authTTL := time.Duration(0)
-
 	if args.AuthTTL != "session" {
 		ttl, err := time.ParseDuration(args.AuthTTL)
 		if err != nil {
@@ -210,10 +201,8 @@ func createServer(args cli.Args, hostService web.HostService) *http.Server {
 
 	if _, ok := os.LookupEnv("LIVE_FS"); ok {
 		if dev {
-			log.Info().Msg("Using live filesystem at ./public")
 			assets = os.DirFS("./public")
 		} else {
-			log.Info().Msg("Using live filesystem at ./dist")
 			assets = os.DirFS("./dist")
 		}
 	}
